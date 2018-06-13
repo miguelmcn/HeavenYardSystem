652386541c18f37edb727eb487f0df2dd075cad5
Maurício M. Rossi <38935520+mauricio-rossi@users.noreply.github.com>
2018-06-13 15:10:23 -0300

    Handler Familia - putFamIdR

--- src/Handler/Familia.hs
+++ src/Handler/Familia.hs
@@ -13,3 +13,10 @@ getFamCemiIdR cid = do
     addHeader "Access-Control-Allow-Origin" "*"
     familiasCemiterio <- runDB $ selectList [FamiliaCemiterioId ==. cid] []
     sendStatusJSON ok200 (object ["resp" .= familiasCemiterio])
+    
+putFamIdR :: FamiliaId -> Handler Value
+putFamIdR fid = do
+    addHeader "Access-Control-Allow-Origin" "*"
+    familia <- requireJsonBody :: Handler Familia
+    runDB $ replace fid familia
+    sendStatusJSON ok200 (object ["resp" .= fromSqlKey fid])    
